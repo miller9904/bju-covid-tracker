ba5ac329858475ea03c867849cfc32f9524396e4
miller9904 <miller9904@gmail.com>
2020-12-18 16:52:05 -0500

    Setting up wsgi & systemd config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-# Create a new virtual environment
-python3 -m venv venv
 # Switch to virtual environment
 . venv/bin/activate
 
 # Install Python requirements
 pip3 install -r requirements.txt
+
+# Leave virtual environment
+deactivate
+
+sudo sed -e "s/{username}/${1}/g" bjucovid.service.template > /etc/systemd/system/bjucovid.service
+
+sudo systemctl start bjucovid
+sudo systemctl enable bjucovid
+
+sudo systemctl status bjucovid
