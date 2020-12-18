584a00340a0ea85bc415944c0a559a894db79686
miller9904 <miller9904@gmail.com>
2020-12-18 17:12:30 -0500

    setup.sh redirect to tee

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ pip3 install -r requirements.txt
 # Leave virtual environment
 deactivate
 
-sudo sed -e "s/{username}/${1}/g" bjucovid.service.template > /etc/systemd/system/bjucovid.service
+sed -e "s/{username}/${1}/g" bjucovid.service.template | sudo tee /etc/systemd/system/bjucovid.service
 
 sudo systemctl start bjucovid
 sudo systemctl enable bjucovid
